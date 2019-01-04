ab39bb5f58f9d85061b82b201b0f4275e9f586ed
turbinewpu <46360878+turbinewpu@users.noreply.github.com>
2019-01-03 19:51:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function getinfo() {
+    function getinfo() {
 var xmlhttp = new XMLHttpRequest();
        xmlhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
            var myObj = JSON.parse(this.responseText);
-           document.getElementById("owner").innerhtml=myObj;
+           document.getElementById("1").innerHTML=myObj;
          }
        };
        xmlhttp.open("GET","https://turbinecc.github.io/controllerinfo/" + document.getElementById("urlbar").value +".json", true);
